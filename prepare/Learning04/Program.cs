a7cfeeb049ed506ca8de63ef50e790fc6065dbe8
vbisso <valeriabisso19@gmail.com>
2024-02-22 11:36:23 -0700

    learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment newAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(newAssignment.GetSummary());
+
+        MathAssignment newMathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.1", "Problems 1-5");
+        Console.WriteLine(newMathAssignment.GetSummary());
+        Console.WriteLine(newMathAssignment.GetHomeworkList());
+
+        WritingAssignment newWritingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(newWritingAssignment.GetSummary());
+        Console.WriteLine(newWritingAssignment.GetWritingInformation());
     }
 }
